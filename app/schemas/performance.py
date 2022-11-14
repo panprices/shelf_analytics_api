b8a5ba56a2131b6eb60a80a40ab2aa935e2354b1
damianr13 <robert-andrei.damian@panprices.com>
2022-11-14 21:17:23 +0100

    Added individual performance endpoints

--- app/schemas/performance.py
+++ app/schemas/performance.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import Union, List, Optional
+from typing import Union, List, Optional, Dict
 
 from pydantic import BaseModel, Field
 
@@ -14,7 +14,9 @@ class CategorySplit(BaseModel):
 class CategoryPerformance(BaseModel):
     category_name: str = Field(description="The name of the category")
     category_id: Union[str, uuid.UUID] = Field(description="The id of the category")
-    total_products: int = Field(description="The total number of products in the category")
+    total_products: int = Field(
+        description="The total number of products in the category"
+    )
 
     split: List[CategorySplit] = Field(
         description="The brands visible in the category with their share"
@@ -25,3 +27,15 @@ class RetailerPerformance(BaseModel):
     categories: List[CategoryPerformance] = Field(
         description="The split per category for the current retailer"
     )
+
+
+class IndividualRetailerCategoryPerformanceDetails(BaseModel):
+    products_count: int = Field(description="Number of products in the category")
+    page_size: int = Field(description="Number of products per page")
+    full_name: str = Field(description="Full name with all sub categories in path")
+
+
+class RetailersCategoryPerformanceDetails(BaseModel):
+    categories: Dict[
+        Union[str, uuid.UUID], IndividualRetailerCategoryPerformanceDetails
+    ] = Field(description="A dict where the key is the retailer id")
