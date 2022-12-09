334c5d9242c1489e5c714b162c5c7592133ded8f
Toan Phan <toanphan.dev@gmail.com>
2022-12-09 16:20:58 +0100

    perf: improve query speed

--- app/crud/retailers.py
+++ app/crud/retailers.py
@@ -51,7 +51,7 @@ def get_categories_split(
         from (
             select category_id, brand, COUNT(*) as product_count, is_customer from (
                 select rp.id, rp.category_id as category_id, 
-                    (b.id = :brand_id OR rp.brand = (SELECT name from brand WHERe id = :brand_id)) as is_customer, 
+                    (b.id = :brand_id OR rp.brand = (SELECT name from brand WHERE id = :brand_id)) as is_customer, 
                     CASE 
                         WHEN b.id = :brand_id then b.name
                         WHEN rp.brand is NULL then 'No brand'
@@ -64,7 +64,7 @@ def get_categories_split(
                 where rp.retailer_id = :retailer_id
             ) rp_brand_fixed
             where category_id in (
-                select category_id 
+                select distinct category_id 
                 from retailer_product rp 
                     join product_matching pm on rp.id = pm.retailer_product_id 
                 where rp.retailer_id = :retailer_id
