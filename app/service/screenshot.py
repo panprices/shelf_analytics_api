778e28f4325052bf5514daf4309d06b5ef55fb55
damianr13 <robert-andrei.damian@panprices.com>
2024-04-25 16:28:13 +0200

    Fix: Fix screenshot check on big exports

--- app/service/screenshot.py
+++ app/service/screenshot.py
@@ -22,6 +22,7 @@ async def __retrieve_screenshot_url(
         url_hash = hashlib.md5(str(product_url).encode()).hexdigest()
         tentative_screenshot_url = f"https://storage.googleapis.com/b2b_shelf_analytics_images/screenshots/{url_hash}.jpg"
 
+        logger.debug(f"Retrieving screenshot URL for {product_url}...")
         head_response = await client.head(tentative_screenshot_url)
         return tentative_screenshot_url if head_response.status_code == 200 else None
     except httpx.HTTPError as exc:
@@ -33,12 +34,14 @@ async def __retrieve_screenshot_url(
 
 async def assign_screenshot_url(
     client: httpx.AsyncClient,
+    semaphore: asyncio.Semaphore,
     product: TRetailerProductModel,
     output_model_class: Type[TRetailerProductSchema],
 ) -> TRetailerProductSchema:
-    result = output_model_class.from_orm(product)
-    result.screenshot_url = await __retrieve_screenshot_url(client, product.url)
-    return result
+    async with semaphore:
+        result = output_model_class.from_orm(product)
+        result.screenshot_url = await __retrieve_screenshot_url(client, product.url)
+        return result
 
 
 async def preprocess_retailer_offers(
@@ -46,6 +49,13 @@ async def preprocess_retailer_offers(
     output_model_class: Type[TRetailerProductSchema],
 ) -> List[TRetailerProductSchema]:
     # Check for the screenshots in parallel
-    async with httpx.AsyncClient() as client:
-        tasks = [assign_screenshot_url(client, p, output_model_class) for p in products]
+    concurrency = 100
+    semaphore = asyncio.Semaphore(concurrency)
+    async with httpx.AsyncClient(
+        limits=httpx.Limits(max_connections=concurrency),
+    ) as client:
+        tasks = [
+            assign_screenshot_url(client, semaphore, p, output_model_class)
+            for p in products
+        ]
         return list(await asyncio.gather(*tasks))
