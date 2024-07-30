1500832ec2fca07c5b1bdb69731920446e496a74
Tom Dickson <tom@tomdickson.eu>
2024-07-30 16:58:09 +0200

    Added iteration to the test on the external API.

--- tests/test_external_v2.py
+++ tests/test_external_v2.py
@@ -2,6 +2,7 @@ import pytest
 from fastapi.testclient import TestClient
 from app.main import app  # Import the FastAPI app instance
 from benchmark.config import BASE_URL
+import random
 
 client = TestClient(app)
 
@@ -9,14 +10,15 @@ client = TestClient(app)
 # TODO: Don't send an API key to GitHub. Since it is sandbox, it is fine for now.
 sandbox_api_key = "loupe_3dnXjnT4xc24KAoqFbiPKI0JjHhnuVyoY5098kZDGuwo40BP"
 
-def test_get_retailer_offers_no_filters():
+def get_retailer_offers_no_filters_in_test(page=None):
     # Send the request with authentication headers
     response = client.get(
         f"{BASE_URL}/v2/products/retailer_offers",
         headers= {
             "x-api-key": sandbox_api_key,
             "Accept": "application/json",
-        }
+        },
+        params={"page": page} if page is not None else {}
     )
 
     # Print the response content for debugging
@@ -46,3 +48,20 @@ def test_get_retailer_offers_no_filters():
         assert "id" in data["rows"][0]
         assert "name" in data["rows"][0]
         assert "available_at_retailer" in data["rows"][0]
+    
+    return {
+        "rows": data["rows"],
+        "count": data["count"],
+        "page": data["page"],
+        "pages_count": data["pages_count"],
+    }
+
+def test_get_retailer_offers_no_filters(page=None):
+    max_pages = random.randint(1, 7)
+    response = get_retailer_offers_no_filters_in_test(page)
+    if response["page"] >= max_pages:
+        return
+    elif response["page"] == response["pages_count"]:
+        return
+    else:
+        test_get_retailer_offers_no_filters(page=response["page"] + 1)
